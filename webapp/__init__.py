5a649810961b53c85339da34cd951a542a0b1cbe
CyramaeM <127583307+CyramaeM@users.noreply.github.com>
2025-07-28 13:31:51 +0000

    Fix Cloudinary API key configuration to use environment variable

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -24,7 +24,7 @@ def create_app():
 
     cloudinary.config(
         cloud_name=CLOUDINARY_CLOUD_NAME,
-        api_key=875432953529479,
+        api_key=CLOUDINARY_API_KEY,
         api_secret=CLOUDINARY_API_SECRET
         )
     mysql.init_app(app)
